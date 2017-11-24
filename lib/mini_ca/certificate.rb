1ba305353c01b0b40260b48d873a94bf3723969d
Thomas Orozco <thomas@orozco.fr>
2017-11-24 13:21:26 +0100

    Include lib

---
+++ lib/mini_ca/certificate.rb
@@ -0,0 +1,129 @@
+module MiniCa
+  class Certificate
+    DIGEST = OpenSSL::Digest::SHA256
+
+    attr_reader :key, :x509, :issuer, :ca
+
+    # rubocop:disable ParameterLists
+    def initialize(
+      cn,
+      sans: nil,
+      issuer: nil,
+      ca: false,
+      serial: nil,
+      not_before: nil,
+      not_after: nil,
+      country: nil,
+      state: nil,
+      location: nil,
+      organization: nil
+    )
+      @key = OpenSSL::PKey::RSA.new(2048)
+      @x509 = OpenSSL::X509::Certificate.new
+      @issuer = issuer
+      @ca = ca
+      @counter = 0
+
+      x509.version = 0x2
+      x509.serial = serial || 0
+
+      x509.public_key = key.public_key
+
+      x509.subject = OpenSSL::X509::Name.new
+
+      [
+        ['CN', cn],
+        ['C', country],
+        ['ST', state],
+        ['L', location],
+        ['O', organization]
+      ].each do |prop, value|
+        next if value.nil?
+        x509.subject = x509.subject.add_entry(prop, value)
+      end
+
+      x509.issuer = issuer ? issuer.x509.subject : x509.subject
+
+      if issuer
+        not_before ||= issuer.x509.not_before
+        not_after ||= issuer.x509.not_after
+
+        if issuer.x509.not_before > not_before
+          raise Error, 'Certificate cannot become valid before issuer'
+        end
+
+        if issuer.x509.not_after < not_after
+          raise Error, 'Certificate cannot expire after issuer'
+        end
+      else
+        not_before ||= Time.now - 3600 * 24
+        not_after ||= Time.now + 3600 + 24
+      end
+
+      x509.not_before = not_before
+      x509.not_after = not_after
+
+      ef = OpenSSL::X509::ExtensionFactory.new
+      ef.subject_certificate = x509
+
+      sans = (sans || []) + ["DNS:#{cn}"]
+
+      exts = if ca
+               [
+                 ef.create_extension('basicConstraints', 'CA:TRUE', true)
+               ]
+             else
+               [
+                 ef.create_extension('basicConstraints', 'CA:FALSE', true),
+                 ef.create_extension('subjectAltName', sans.join(','), false)
+               ]
+             end
+
+      exts.each { |e| x509.add_extension(e) }
+
+      signing_key = issuer ? issuer.key : key
+      x509.sign signing_key, DIGEST.new
+    end
+    # rubocop:enable ParameterLists
+
+    def issue(cn, **opts)
+      raise 'CA must be set to use #issue' unless ca
+      @counter += 1
+      Certificate.new(cn, issuer: self, serial: @counter, **opts)
+    end
+
+    def store
+      raise 'CA must be set to use #store' unless ca
+      OpenSSL::X509::Store.new.tap { |store| store.add_cert(x509) }
+    end
+
+    def chain
+      bits = []
+      this_cert = self
+      until (this_cert = this_cert.issuer).nil?
+        bits << this_cert
+      end
+      bits[0...-1]
+    end
+
+    def bundle
+      [self] + chain
+    end
+
+    def x509_pem
+      x509.to_s
+    end
+
+    def chain_pem
+      chain.map(&:x509).map(&:to_s).join('')
+    end
+
+    def bundle_pem
+      bundle.map(&:x509).map(&:to_s).join('')
+    end
+
+    def key_pem
+      key.to_s
+    end
+  end
+end
---
+++ spec/mini_ca/certificate_spec.rb
@@ -0,0 +1,133 @@
+require 'spec_helper'
+
+describe MiniCa::Certificate do
+  describe '#initialize' do
+    it 'initializes a self-signed certificate' do
+      c = described_class.new('name')
+      expect(c.x509.subject.to_s).to eq('/CN=name')
+      expect(c.x509.issuer.to_s).to eq('/CN=name')
+    end
+
+    it 'initializes a CA certificate' do
+      c1 = described_class.new('foo', ca: true)
+      c2 = c1.issue('bar')
+      expect(c1.store.verify(c2.x509)).to be_truthy
+    end
+
+    it 'initializes a certificate with a serial' do
+      c1 = described_class.new('foo', serial: 10)
+      expect(c1.x509.serial).to eq(10)
+    end
+
+    it 'initializes a certificate with not_before' do
+      t = Time.at((Time.now - 100).to_i)
+      c1 = described_class.new('foo', not_before: t)
+      expect(c1.x509.not_before).to eq(t)
+    end
+
+    it 'initializes a certificate with not_after' do
+      t = Time.at((Time.now + 100).to_i)
+      c1 = described_class.new('foo', not_after: t)
+      expect(c1.x509.not_after).to eq(t)
+    end
+
+    context 'subject fields' do
+      it 'sets country' do
+        expect(described_class.new('x', country: 'bar').x509.subject.to_s)
+          .to eq('/CN=x/C=bar')
+      end
+
+      it 'sets state' do
+        expect(described_class.new('x', state: 'bar').x509.subject.to_s)
+          .to eq('/CN=x/ST=bar')
+      end
+
+      it 'sets location' do
+        expect(described_class.new('x', location: 'bar').x509.subject.to_s)
+          .to eq('/CN=x/L=bar')
+      end
+
+      it 'sets organization' do
+        expect(described_class.new('x', organization: 'bar').x509.subject.to_s)
+          .to eq('/CN=x/O=bar')
+      end
+    end
+  end
+
+  context 'CA' do
+    subject { described_class.new('MyCA', ca: true) }
+
+    describe '#issue' do
+      it 'issues signed certificates with a valid serial' do
+        c1 = subject.issue('c1')
+        c2 = subject.issue('c2')
+        expect(c1.x509.serial).not_to eq(c2.x509.serial)
+      end
+
+      it 'fails if the CA becomes valid after the certificate' do
+        t = subject.x509.not_before - 100
+        expect { subject.issue('c', not_before: t) }
+          .to raise_error(/cannot become valid before issuer/i)
+      end
+
+      it 'fails if the CA expires before the certificate' do
+        t = subject.x509.not_after + 100
+        expect { subject.issue('c', not_after: t) }
+          .to raise_error(/cannot expire after issuer/i)
+      end
+    end
+
+    describe '#store' do
+      it 'returns a store trusting the CA' do
+        alt = described_class.new('OtherCA', ca: true)
+        cert = subject.issue('Client')
+
+        expect(subject.store.verify(cert.x509)).to be_truthy
+        expect(alt.store.verify(cert.x509)).to be_falsey
+      end
+    end
+  end
+
+  describe '#chain / #bundle' do
+    it 'returns nothing for a self-signed certificate' do
+      c = described_class.new('c')
+      expect(c.chain).to be_empty
+    end
+
+    it 'returns nothing for a certificate issued by a CA' do
+      ca = described_class.new('ca', ca: true)
+      c = ca.issue('c')
+      expect(c.chain).to be_empty
+      expect(c.bundle).to eq([c])
+    end
+
+    it 'returns 1 leaf certificate for 1 intermediate' do
+      ca = described_class.new('ca', ca: true)
+      i1 = ca.issue('i1', ca: true)
+      c = i1.issue('c')
+      expect(c.chain).to eq([i1])
+      expect(c.bundle).to eq([c, i1])
+    end
+
+    it 'returns 2 leaf certificates for 2 intermediates' do
+      ca = described_class.new('ca', ca: true)
+      i1 = ca.issue('i1', ca: true)
+      i2 = i1.issue('i2', ca: true)
+      c = i2.issue('c')
+      expect(c.chain).to eq([i2, i1])
+      expect(c.bundle).to eq([c, i2, i1])
+    end
+  end
+
+  describe '#chain_pem / #bundle_pem' do
+    it 'returns the certificate chain' do
+      ca = described_class.new('ca', ca: true)
+      i1 = ca.issue('i1', ca: true)
+      c = i1.issue('c')
+      expect(c.chain_pem.split("\n").grep(/BEGIN CERTIFICATE/).size).to eq(1)
+      expect(c.bundle_pem.split("\n").grep(/BEGIN CERTIFICATE/).size).to eq(2)
+      expect(c.bundle_pem).to start_with(c.x509_pem)
+      expect(c.bundle_pem).to end_with(i1.x509_pem)
+    end
+  end
+end
